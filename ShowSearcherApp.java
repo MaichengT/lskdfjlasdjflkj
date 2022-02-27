aba973c87b4a72704a008dece429eb4d7e9e244b
Braeden Noah Bertz <34774967+BraedenBertz@users.noreply.github.com>
2022-02-27 11:17:20 -0600

    Update ShowSearcherApp.java

--- ShowSearcherApp.java
+++ ShowSearcherApp.java
@@ -5,6 +5,9 @@
 // Lecture #: 004 @4:00pm
 // Notes to Grader: Algorithm Engineer did not communicate with me, Frontend Engineer did
 
+import java.io.FileNotFoundException;
+import java.util.List;
+
 /**
  * Implementation of the app for the ShowSearcher project. Used to start the program
  */
@@ -15,11 +18,15 @@ public class ShowSearcherApp {
      * @param args args
      */
     public static void main(String[] args) {
-//        IShowLoader loader = new ShowLoader(); //new ShowLoader();
-//        List<IShow> shows = loader.loadShows("data/tv_shows.csv");
-//        ShowSearcherBackend backend = new ShowSearcherBackend(); //new ShowSearcherBackend();
-//        for(IShow show : shows) backend.addShow(show);
-//        ShowSearcherFrontend frontend = new ShowSearcherFrontend(backend); //new ShowSearcherFrontend(backend);
-//        frontend.runCommandLoop();
+        IShowLoader loader = new ShowLoader(); //new ShowLoader();
+        try {
+            List<IShow> shows = loader.loadShows("data/tv_shows.csv");
+            ShowSearcherBackend backend = new ShowSearcherBackend(); //new ShowSearcherBackend();
+            for (IShow show : shows) backend.addShow(show);
+            ShowSearcherFrontend frontend = new ShowSearcherFrontend(backend); //new ShowSearcherFrontend(backend);
+            frontend.runCommandLoop();
+        } catch (FileNotFoundException e) {
+            System.exit(-1);
+        }
     }
 }
